413aa19d58f0579fb4e955884c00e70979ed5d92
Hunter Fuller <hfuller@pixilic.com>
2015-07-07 00:28:59 -0500

    titles work

--- radio.js
+++ radio.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+	$('#player-ui').hide(0); //hide the player until we know there is a cast
+	//why didn't I just have it hidden to begin with from the css?
+	//in case the user doesn't have javascript!
+	
 	$('#status').html("Loading player...");
 	var stream = {
 		title: "dr3v Radio",
@@ -34,16 +38,29 @@ $(document).ready(function(){
 	});
 
 	function refreshTitle() {
+		console.log("Going for current status");
 		$.ajax({
 			url:"cast/json.xsl?mount=/stream",
 			dataType:"json",
 			minmeType:"text/json",
 			cache:false
 		}).done(function(data){
-			var title = data.sources[0].title;
-			console.log(title);
-			$('#title').html(title);
-			console.log('Ok');
+			if ( data.sources.length > 0 ) {
+				//there is someone streaming
+				var title = data.sources[0].title;
+				var desc = data.sources[0].description;
+				console.log(title);
+				$('#title').html(title);
+				$('#status').html(desc);
+				console.log(desc);
+				$('#player-ui').show(400);
+				console.log('Ok');
+			} else {
+				$('#player-ui').hide(400);
+				console.log("Not broadcasting");
+				$('#status').html("Sorry we missed&nbsp;you!");
+				$('#title').html("There is no DJ broadcasting right now.");
+			}
 		});
 		setTimeout(refreshTitle, 10*1000);
 	};
